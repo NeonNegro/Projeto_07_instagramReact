d786bc8e21ec004ece3cd4d89e308ac2bc9d2c84
thiagoribb <thiribeiro142@gmail.com>
2021-12-09 15:34:34 -0300

    Preparando revisão de código

--- src/components/FundoMobile.js
+++
@@ -1,7 +0,0 @@
-import Icons from './Icons'
-
-export default function FundoMobile(){
-    return  <Icons style='fundo-mobile' icons={['home', 'search-outline','add-circle-outline', 'heart-outline', 'person-outline']} />
-}
-
-
